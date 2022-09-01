6c08ed11c292ff2c12b3e797396a26c4eaa39686
rymdpeter <pka.khl@gmail.com>
2022-09-01 14:52:16 +0200

    new header and updated secrets

--- components/Intro/Intro.tsx
+++ components/Intro/Intro.tsx
@@ -6,7 +6,7 @@ import styles from './Intro.module.scss';
 const Intro: React.FC = () => (
   <section className={styles.container}>
     <Typography variant="h1" className={styles.title}>
-      Next.js WordPress boilerplate.
+      Headless Nameless
     </Typography>
   </section>
 );
